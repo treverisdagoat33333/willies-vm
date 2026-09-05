74aa3198afd8313193ea847f2133eac18e94969e
treverisdagoat33333 <treverwalkins096@gmail.com>
2026-09-05 02:51:06 -0500

    Refactor service worker to use controller.sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,16 +1,15 @@
-importScripts("/controller/controller.api.js");
+importScripts("/controller/controller.sw.js");
 
-self.addEventListener("fetch", (event) => {
-	if ($scramjetController.shouldRoute(event)) {
-		event.respondWith($scramjetController.route(event));
-		return;
-	}
+self.addEventListener("install", () => {
+  self.skipWaiting();
 });
 
-self.addEventListener("install", () => self.skipWaiting());
-self.addEventListener("activate", (event) =>
-	event.waitUntil(self.clients.claim())
-);
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
 
-self.addEventListener("message", () => {
+self.addEventListener("fetch", (event) => {
+  if ($scramjetController.shouldRoute(event)) {
+    event.respondWith($scramjetController.route(event));
+  }
 });
