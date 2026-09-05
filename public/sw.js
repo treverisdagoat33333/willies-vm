c0887080bbbc0663f4afed58db5f2d99400d4870
treverisdagoat33333 <treverwalkins096@gmail.com>
2026-09-05 00:36:30 -0700

    Refactor fetch event listener in service worker

--- public/sw.js
+++ public/sw.js
@@ -1,16 +1,14 @@
 importScripts("/controller/controller.sw.js");
 
 self.addEventListener("fetch", (event) => {
-	if ($scramjetController.shouldRoute(event)) {
-		event.respondWith($scramjetController.route(event));
-		return;
-	}
+  if ($scramjetController.shouldRoute(event)) {
+    event.respondWith($scramjetController.route(event));
+  }
 });
 
 self.addEventListener("install", () => self.skipWaiting());
 self.addEventListener("activate", (event) =>
-	event.waitUntil(self.clients.claim())
+  event.waitUntil(self.clients.claim())
 );
 
-self.addEventListener("message", () => {
-});
+self.addEventListener("message", () => {});
